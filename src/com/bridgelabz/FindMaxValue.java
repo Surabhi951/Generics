ce7aad4fe8e00994e97687228c45c2b43bae875a
Surabhi951 <surabhibhagat95@gmail.com>
2022-11-22 20:25:18 +0530

    Modified: Find maximum value between 3 floats number

--- src/com/bridgelabz/FindMaxValue.java
+++ src/com/bridgelabz/FindMaxValue.java
@@ -13,7 +13,20 @@ public class FindMaxValue {
         return max;
     }
 
+    public static Float testMaximum(Float x, Float y, Float z) {
+        Float max = x;
+        if(y.compareTo(max) > 0) {
+            max = y;
+        }
+        if(z.compareTo(max) > 0) {
+            max = z;
+        }
+        return max;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("The maximum value between 3 integer is: " + testMaximum(30,20,10));
+        System.out.println("The maximum value between 3 float is: " + testMaximum(1.5f,3.5f,2.7f));
     }
 }
