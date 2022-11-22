a02a88c9012a0dfbe554f2c329c82d9a0fc56458
Surabhi951 <surabhibhagat95@gmail.com>
2022-11-22 20:32:10 +0530

    Modified: Find maximum value between 3 strings number

--- src/com/bridgelabz/FindMaxValue.java
+++ src/com/bridgelabz/FindMaxValue.java
@@ -24,9 +24,21 @@ public class FindMaxValue {
         return max;
     }
 
+    public static String testMaximum(String x, String y, String z) {
+        String max = x;
+        if(y.compareTo(max) > 0) {
+            max = y;
+        }
+        if(z.compareTo(max) > 0) {
+            max = z;
+        }
+        return max;
+    }
+
 
     public static void main(String[] args) {
         System.out.println("The maximum value between 3 integer is: " + testMaximum(30,20,10));
         System.out.println("The maximum value between 3 float is: " + testMaximum(1.5f,3.5f,2.7f));
+        System.out.println("The maximum value between 3 String is: " + testMaximum("Apple","Banana","Peach"));
     }
 }
